a26077173ad392018af92b0349a4f25a26ec1064
smoothbronx <vlad.uhwatov@yandex.ru>
2021-05-22 23:28:42 +0200

    Rewritten Kuramoto model, made some changes.

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-import numpy as np
 from models.kuramoto import Kuramoto
-from networkx import binomial_graph as to_binomial, to_numpy_array as to_array
+import networkx as nx
+import numpy as np
 
-matrix = to_array(to_binomial(n=5, p=1))
-print(matrix.T)
+matrix = nx.Graph()
 
-model = Kuramoto(coupling=3, dt=0.01, total_time=100000, nodes_count=len(matrix), vibration_array=[1, 2, 3, 4, 5])
-act_mat = model.run(connectivity_matrix=matrix)
+obj = {'aaa': 10, 'bbb': 10, 'ccc': .01,
+       'aab': 50, 'abc': 1, 'fdsfs': 0.12,
+       'aba': 20, 'bca': 10, 'dsf': 15}
 
-times = [i for i in range(100000)]
-for time in times:
-    print(act_mat[:, time])
+G = nx.erdos_renyi_graph(n=obj.__len__(), p=1)
+N = G.number_of_nodes()
+print(N)
+fps = 120
+time = 4500
+L = fps*time
+omega = np.random.uniform(0.95, 1.05, N)
+
+TS = Kuramoto(G, L, dt=1/fps, strength=(1, 3, 3, 5, 6, 7, 8, 8, 10), phases=(2, 3, 1, 1, 2, 3, 4, 5, 6), frequencies=list(obj.values())).simulate()
+
+print(TS[:, 0])
+print(TS[:, -1])
+# [3.79133203 6.2465093  6.2465093]
+
--- models/kuramoto.py
+++ models/kuramoto.py
@@ -1,57 +1,95 @@
-from numpy.random import random, normal
-from numpy import sin, meshgrid, linspace, zeros_like
-from numpy import pi as PI, e as E, sum as numpy_sum
-from scipy.integrate import odeint
+from collections import defaultdict
+
+import networkx as nx
+import numpy as np
+from functools import wraps
+import scipy.integrate as it
+import warnings
+from typing import Any
+
+
+def ensure_unweighted(graph):
+    for _, _, attr in graph.edges(data=True):
+        if not np.isclose(attr.get("weight", 1.0), 1.0):
+            new_graph = graph.__class__()
+            new_graph.add_nodes_from(graph)
+            new_graph.add_edges_from(graph.edges)
+            warnings.warn("Coercing weighted graph to unweighted.", RuntimeWarning)
+            return new_graph
+    return graph
+
+
+class DataLengthException(Exception):
+    def __init__(self, message: str):
+        self.__message = message
+
+    def raise_this(self):
+        raise self
+
+    def __repr__(self):
+        return self.__message
+
+
+def unweighted(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        args = [ensure_unweighted(arg) if issubclass(arg.__class__, nx.Graph) else arg for arg in args]
+        return func(*args, **kwargs)
+    return wrapper
 
 
 class Kuramoto:
-    def __init__(self, coupling: float = 1, dt: float = 0.01, total_time: float = 10, nodes_count=None,
-                 vibration_array=None):
-        if nodes_count is None and vibration_array is None:
-            raise ValueError("nodes_count or vibration_array must be specified")
+    __results = defaultdict(dict)
 
+    @unweighted
+    def __init__(self, graph, time: int = 20, dt: float = 0.01, strength=None, phases=None, frequencies=None):
+        self.graph = graph
         self.dt = dt
-        self.total_time = total_time
-        self.coupling = coupling
+        self.graph_array = nx.to_numpy_array(self.graph)
+        self.nodes_count = self.graph.number_of_nodes()
+        self.time = np.linspace(self.dt, time * self.dt, time)
+        self.array_of_ones = np.ones(self.nodes_count)
 
-        if vibration_array is not None:
-            self.natfreqs = vibration_array
-            self.nodes_count = len(vibration_array)
-        else:
-            self.nodes_count = nodes_count
-            self.natfreqs = normal(size=self.nodes_count)
+        self.strength = (np.array(strength) if strength.__len__() == self.nodes_count else
+                         DataLengthException('Connectivity must be equal to the number of oscillators').raise_this()) \
+            if strength is not None else np.random.uniform(0.1, 10, self.nodes_count)
 
-    def derivative(self, angles_vector, time, connectivity_matrix):
-        assert len(angles_vector) == len(self.natfreqs) == len(
-            connectivity_matrix), 'Input dimensions do not match, check lengths'
-        angles_i, angles_j = meshgrid(angles_vector, angles_vector)
-        dxdt = self.natfreqs + self.coupling / connectivity_matrix.sum(axis=0) * (
-                    connectivity_matrix * sin(angles_j - angles_i)).sum(axis=0)
-        return dxdt
+        self.null_theta = (phases if phases.__len__() == self.nodes_count
+                           else DataLengthException('Phases must be equal to the number of oscillators').raise_this()) \
+            if phases is not None else 2 * np.pi * np.random.rand(self.nodes_count)
 
-    def integrate(self, angles_vector, connectivity_matrix):
-        time = linspace(0, self.total_time, int(self.total_time / self.dt))
-        time_series = odeint(self.derivative, angles_vector, time, args=(connectivity_matrix,))
-        return time_series.T
+        self.omega = (frequencies if frequencies.__len__() == self.nodes_count
+                      else DataLengthException('Frequencies must be equal to the number of oscillators').raise_this()) \
+            if frequencies is not None else np.random.uniform(0.9, 1.1, self.nodes_count)
 
-    def run(self, connectivity_matrix=None, angles_vector=None):
-        assert (connectivity_matrix == connectivity_matrix.T).all(), 'connectivity_matrix must be symmetric'
-        if angles_vector is None:
-            angles_vector = self.init_angles()
-        return self.integrate(angles_vector, connectivity_matrix)
+    def simulate(self):
+        ts_wrapper = it.odeint(self.theta, self.null_theta, self.time, self.strength, self.omega, self.graph_array)
+        ts = np.flipud(ts_wrapper.T) % (2 * np.pi)
 
-    def mean_frequency(self, activity_matrix, connectivity_matrix):
-        assert len(connectivity_matrix) == activity_matrix.shape[0], 'connectivity_matrix does not match act_mat'
-        _, steps_count = activity_matrix.shape
-        dxdt = zeros_like(activity_matrix)
-        for time in range(steps_count):
-            dxdt[:, time] = self.derivative(activity_matrix[:, time], time, connectivity_matrix)
-        return numpy_sum(dxdt * self.dt, axis=1) / self.total_time
+        self.__results["internal_frequencies"] = self.omega
+        self.__results["ground_truth"] = self.graph
+        self.__results["TS"] = ts
+        return ts
 
-    @staticmethod
-    def phase_coherence(angles_vector):
-        total = sum([(E ** (1j * i)) for i in angles_vector])
-        return abs(total / len(angles_vector))
+    def getResults(self):
+        return self.__results
 
-    def init_angles(self):
-        return 2 * PI * random(size=self.nodes_count)
+    def __set_res(self, path: str, obj: Any):
+        return self.__results.__setitem__(path, obj)
+
+    def theta(self, null_theta, time, strength, omega, graph_array):
+        return omega + strength / self.nodes_count * (self.graph_array * np.sin(np.outer(self.array_of_ones, null_theta) - np.outer(null_theta, self.array_of_ones))).dot(self.array_of_ones)
+
+
+# graph!, time added, dt later, strength later, phases=None, frequencies=None
+# class KuramotoHandler:
+#     response = defaultdict(dict)
+#
+#     def __init__(self, data: dict):
+#         self.objects_list = data.get("objects")
+#         self.fps = data.get("fps")
+#         self.time = data.get("time")
+#         self.objects_names = tuple(self.objects_list.keys())
+
+
+
